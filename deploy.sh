164937e30c88ecf6d53ec65a5c76f45fcf174da0
e13q <110967581+e13q@users.noreply.github.com>
2025-01-19 21:39:37 +0300

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 set -e
 VENV_DIR="venv"
-
 echo "Проверка наличия обновлений..."
 git fetch
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
 BASE=$(git merge-base @ @{u})
-
 if [ $LOCAL = $REMOTE ]; then
     echo "Нет новых обновлений в репозитории. Завершение скрипта."
     exit 0
 fi
-
 if [ $LOCAL = $BASE ]; then
     echo "Нужно обновить репозиторий."
 else
     echo "Нужно слить изменения."
     exit 1
 fi
-
 echo "Обновление кода репозитория..."
 git pull
 COMMIT_HASH=$(git rev-parse HEAD)
@@ -33,6 +29,7 @@ docker-compose run --rm backend python manage.py migrate
 echo "Отправка уведомления в Rollbar..."
 ROLLBAR_TOKEN=$(grep 'ROLLBAR_TOKEN' .env | cut -d '=' -f2)
 export ROLLBAR_TOKEN
+docker-compose up -d
 curl https://api.rollbar.com/api/1/deploy/ \
     -F access_token="$ROLLBAR_TOKEN" \
     -F environment="production" \
